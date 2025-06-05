1cbd807d22941e3cfe30b41fffba6943ba3ccf20
Eaditber <eadit.bernstein@gmail.com>
2025-06-05 23:38:00 +0300

    new files

---
+++ airflow-intro/config/paths_config.py
@@ -0,0 +1,7 @@
+import os
+RAW_DIR = "artifacts/raw"
+TRAIN_PATH = os.path.join(RAW_DIR,'customer_data_train.csv')
+TEST_PATH = os.path.join(RAW_DIR,'customer_data_test.csv')
+
+
+PROCESSED_DIR = "artifacts/processed"
---
+++ config/database_config.py
@@ -0,0 +1,8 @@
+
+DB_CONFIG = {
+    'host' : 'localhost',
+    'port' : '5432',
+    'user' : 'airflow',
+    'password' : 'airflow',
+    'dbname' : 'airflow'
+}
